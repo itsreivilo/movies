2eb67d3f661c2c9a9b1a20bbfe66db499579a67f
DASHA <106515359+pouvoirdasha@users.noreply.github.com>
2023-12-11 14:44:24 +0100

    Dasha (#2)
    * changement
    
    * Readme added
    
    * début de fichier scrapping wikipédie
    
    * scrap
    
    * found urls for awards
    
    * started scrapping function
    
    * travail sur la fonction
    
    * conversion des tables dans les listes
    
    * réussi à faire des listes
    
    ---------
    
    Co-authored-by: dharyfullina <dariia.haryfullina@ensae.fr>

---
+++ scrap_wiki.py
@@ -0,0 +1,36 @@
+# !pip install -q lxml à executer 
+import bs4
+import lxml
+import pandas
+import urllib
+
+from urllib import request
+#Enregistrement des URL à scrapper dans notre projet (listes des nominés et gagnants des grand prix du cinéma - oscar, césar, cannes et venise)
+url_academy_bf = 'https://en.wikipedia.org/wiki/Academy_Award_for_Best_Picture'
+url_academy_bd = 'https://en.wikipedia.org/wiki/Academy_Award_for_Best_Director'
+url_academy_bc = 'https://en.wikipedia.org/wiki/Academy_Award_for_Best_Director'
+url_cesar_bf = 'https://en.wikipedia.org/wiki/C%C3%A9sar_Award_for_Best_Film'
+url_cesar_bd = 'https://en.wikipedia.org/wiki/C%C3%A9sar_Award_for_Best_Director'
+url_palme_or = 'https://en.wikipedia.org/wiki/Palme_d%27Or'
+url_cannes_jury = 'https://en.wikipedia.org/wiki/Jury_Prize_(Cannes_Film_Festival)'
+url_cannes_gp = 'https://en.wikipedia.org/wiki/Grand_Prix_(Cannes_Film_Festival)'
+url_lion_or = 'https://en.wikipedia.org/wiki/Golden_Lion'
+url_lion_argent = 'https://en.wikipedia.org/wiki/Silver_Lion'
+url_venice_gj = 'https://en.wikipedia.org/wiki/Grand_Jury_Prize_(Venice_Film_Festival)'
+
+url_req = request.urlopen(url_academy_bf).read()
+wiki_page = bs4.BeautifulSoup(url_req, "lxml")
+
+
+
+#fonction qui prend en argument le URL et qui ressort le data_frame avec les tableaux correspondants 
+
+def scrapper_wiki(url): 
+    url_req = request.urlopen(url).read()
+    wiki_page = bs4.BeautifulSoup(url_req, "lxml")
+
+
+
+
+
+
